bb1078f522fd982b0ee05993135d907af910997c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-12-10 16:12:20 -0300

    feat(req04): implementa get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,14 @@
 from typing import Union, List, Dict
+from src.insights.jobs import (read)
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    return max([
+        int(job['max_salary'])
+        for job in jobs_list
+        if job['max_salary'].isdigit()
+    ])
 
 
 def get_min_salary(path: str) -> int:
