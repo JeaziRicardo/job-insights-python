e963b986077286bc309ada6dd97ad269818d95a7
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-12-10 22:08:25 -0300

    feat(req09): implementa filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -33,18 +33,11 @@ def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    filter_job = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filter_job.append(job)
+        except ValueError:
+            pass
+    return filter_job
