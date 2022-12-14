18f472c00e531bac3c96968ff61a39b0a6652aa9
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-12-13 22:33:25 -0300

    feat(req10): implementa testes de count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,21 @@
-# from src.pre_built.counter import count_ocurrences
+from pytest import raises
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "jobs") == 306
+
+    assert count_ocurrences("data/jobs.csv", "jOBs") == 306
+
+    assert count_ocurrences("data/jobs.csv", "xablau") == 0
+
+    assert count_ocurrences("data/jobs.csv", "XablaU") == 0
+
+    with raises(TypeError):
+        count_ocurrences()
+
+    with raises(AttributeError):
+        count_ocurrences("data/jobs.csv", 28)
+
+    with raises(FileNotFoundError):
+        count_ocurrences("dat/jobs.csv", "jobs")
